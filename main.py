e84c8b1a806087e78f1d1292f358c57231f1771a
4L R3DOW4N <94780381+al-redowan@users.noreply.github.com>
2022-04-27 11:42:00 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import socket
+import requests
+import sys
+file = open('wp-contentthemessitein.txt').read().split()
+for url in file:
+    domain = url.split('//')[1].replace('www.', '').replace('/', '')
+    try :
+        ipadrre = socket.gethostbyname(domain)
+        response = requests.get(f"https://sonar.omnisint.io/reverse/{ipadrre}").txt
+        for urldata in response:
+            a=open('urllist.txt','a').write(urldata+"\n")
+            a.close()
+    except:
+        pass    
